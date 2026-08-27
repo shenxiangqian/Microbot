315370f261c2c31139cff1c4a27e5c8258356524
Administrator <736718447@qq.com>
2026-08-27 22:26:20 +0800

    test update

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/testscript/TestScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/testscript/TestScript.java
@@ -2,27 +2,36 @@ package net.runelite.client.plugins.microbot.testscript;
 
 import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.EquipmentInventorySlot;
 import net.runelite.client.plugins.microbot.Microbot;
+import net.runelite.client.plugins.microbot.SDK.BankTask.BankAmount;
+import net.runelite.client.plugins.microbot.SDK.BankTask.BankTask;
+import net.runelite.client.plugins.microbot.SDK.BankTask.EquipmentReq;
 import net.runelite.client.plugins.microbot.SDK.Entity.WidgetModel;
 import net.runelite.client.plugins.microbot.SDK.Query.Query;
 import net.runelite.client.plugins.microbot.Script;
-import net.runelite.client.plugins.microbot.api.tileobject.models.Rs2TileObjectModel;
 import net.runelite.client.plugins.microbot.util.widget.Rs2Widget;
 
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class TestScript extends Script {
-    public boolean run(){
-        mainScheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(()->{
-            if(!Microbot.isLoggedIn())return;
-            if(!super.run())return;
+    public boolean run() {
+        mainScheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(() -> {
+            if (!Microbot.isLoggedIn()) return;
+            if (!super.run()) return;
 
-            //Query.object().nameEquals("Tree").findFirst().ifPresent(Rs2TileObjectModel::click);
-            Query.widget().inRoots(465,7).actionContains("Create <col=ff9040>Buy</col> offer").isVisible().findFirst().ifPresent(WidgetModel::click);
-            int cc=0;
+            log.info("{}", Microbot.getClient().getFPS());
 
-        },0,600, TimeUnit.MILLISECONDS);
+            Query.widget().actionContains("Enter name").findFirst().ifPresent(WidgetModel::click);
+
+            var task = BankTask.builder()
+                    .addInvItem(563, BankAmount.of(10))
+                    .addEquipmentItem(EquipmentReq.slot(EquipmentInventorySlot.WEAPON).item(666, BankAmount.of(1)))
+                    .build();
+            if (!task.isSatisfied()) task.execute();
+
+        }, 0, 600, TimeUnit.MILLISECONDS);
         return true;
     }
 
