e9b38167562b975ff3647f79815a8614df6bbe33
shenxiangqian <shenxq@qq.com>
2026-08-28 13:38:46 +0800

    移除SDK相关代码

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/testscript/TestScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/testscript/TestScript.java
@@ -1,13 +1,8 @@
 package net.runelite.client.plugins.microbot.testscript;
 
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.client.plugins.microbot.Microbot;
-import net.runelite.client.plugins.microbot.SDK.Entity.WidgetModel;
-import net.runelite.client.plugins.microbot.SDK.Query.Query;
 import net.runelite.client.plugins.microbot.Script;
-import net.runelite.client.plugins.microbot.api.tileobject.models.Rs2TileObjectModel;
-import net.runelite.client.plugins.microbot.util.widget.Rs2Widget;
 
 import java.util.concurrent.TimeUnit;
 
@@ -19,8 +14,7 @@ public class TestScript extends Script {
             if(!super.run())return;
 
             //Query.object().nameEquals("Tree").findFirst().ifPresent(Rs2TileObjectModel::click);
-            Query.widget().inRoots(465,7).actionContains("Create <col=ff9040>Buy</col> offer").isVisible().findFirst().ifPresent(WidgetModel::click);
-            int cc=0;
+//            int cc=0;
 
         },0,600, TimeUnit.MILLISECONDS);
         return true;
